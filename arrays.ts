be9567ddbd0e69448aae10e34d4b215f56828f8c
cel3ntano <adriano2024celentano@gmail.com>
2024-10-28 19:04:05 +0100

    arrays

---
+++ arrays.ts
@@ -0,0 +1,15 @@
+const departments: string[] = ["dev", "design", "marketing"];
+const department = departments[0];
+// departments.push(5); // error
+
+const report = departments
+  .filter((dept: string) => dept !== "dev")
+  .map((dept: string) => `${dept} - done`);
+
+const nums: number[][] = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+
+const [first] = report;
+console.log("first: ", first);
